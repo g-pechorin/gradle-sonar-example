4715aecb5ab5641fdb1c1342c4374da79c908d5d
Peter A LaValle <peter.lavalle@nottingham.ac.uk>
2016-09-07 17:05:26 +0100

    PAL remade the project for something else

--- src/hello/headers/Msg.h
+++
@@ -1,5 +0,0 @@
-#ifdef ENTERPRISE
-static const char * EDITION = "Enterprise";
-#else
-static const char * EDITION = "Community";
-#endif
--- src/main/cpp/main.cpp
+++
@@ -1,8 +0,0 @@
-#include "Hello.h"
-
-int main(int argc, char ** argv)
-{
-	Hello hello ("Pepito");
-	hello.sayHello(10);
-	return 0;
-}
